c3147b3f5242bd5f09d7c1682d168f738e8808c7
Mohit S <svenkatmohit2883@gmail.com>
2025-05-29 03:45:53 +0530

    semantic relevance calculator file

---
+++ weatherrelevance.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer, util
+
+df = pd.read_csv("weather_wiki_results_mistral.csv")
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def get_similarity(q, r):
+    return util.cos_sim(model.encode(q, convert_to_tensor=True),
+                        model.encode(r, convert_to_tensor=True)).item()
+
+df["semantic_relevance"] = df.apply(lambda row: get_similarity(row["query"], row["final_response"]), axis=1)
+df.to_csv("weather_wiki_results_with_relevance_mistral.csv", index=False)
